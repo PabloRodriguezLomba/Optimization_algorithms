3c4b6855094edcaebf6bd77100b493c5b5a12c6d
Pablorl <Pablorodlom@gmail.com>
2025-03-18 01:50:22 +0100

    feat(main.py): creacion de las funciones calcular_valor_emisora y optimizacion_global

--- main.py
+++ main.py
@@ -16,9 +16,41 @@ def optimizacion_aleatoria(Diccionario):
 
 
 
+def optimizacion_global(Diccionario):
+    lista_solucion = []
+    posible_mejora = True
+    lista_con_estaciones = list(Diccionario)
+    
+    lista_de_valor = []
+    colecion = set()
+    while posible_mejora:
+        lista_de_valor,posible_mejora = calcular_valor_emisoras(colecion,list(Diccionario.values()))
+        siguiente_estacion = 0
+
+        if posible_mejora:
+            posicion_lista = 0
+            for i in range(len(lista_de_valor)):
+                if lista_de_valor[i] > siguiente_estacion:
+                    siguiente_estacion = lista_de_valor[i]
+                    posicion_lista = i
+                
+            estacion_a_añadir = lista_con_estaciones[posicion_lista]        
+            colecion = colecion | Diccionario[estacion_a_añadir]           
+            lista_solucion.append(estacion_a_añadir)
+    return lista_solucion    
 
 
 
+
+def calcular_valor_emisoras(contratadas,potenciales):
+    lista_de_valor = []
+    potencial = False
+    for i in potenciales:
+       valor =  len(i - contratadas)
+       if valor > 0:
+           potencial = True
+       lista_de_valor.append(valor)
+    return lista_de_valor,potencial    
 
 
 
@@ -31,6 +63,9 @@ if "__main__"==__name__:
     
     resultado_minimo_local = optimizacion_aleatoria(lista_estaciones)
 
-    print(f"Este seria el resultado aplicando un algoritmo que busca minimos locales : {resultado_minimo_local}")
+    #print(f"Este seria el resultado aplicando un algoritmo que busca minimos locales : {resultado_minimo_local}")
     
+    resultado_minimo_global = optimizacion_global(lista_estaciones)
+
+    print(resultado_minimo_global)
     
